ac247806ebc00d192300061b6ed3d2bc4e53df0d
arifeld <feldma@students.bialik.vic.edu.au>
2017-11-20 10:37:48 +1100

    Create TeleOP_Full.java

---
+++ TeamCode/TeleOP/TeleOP_Full.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode.TeleOP;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Libraries.LibraryBaseTeleOP;
+
+/**
+ * Created by Ari on 02-11-17.
+ */
+
+
+@TeleOp(name = "TeleOP_Full", group = "Comp")
+public class TeleOP_Full extends LibraryBaseTeleOP {
+
+    private double gyroYaw    = 0;
+    private double dAxial     = 0;
+    private double dLateral   = 0;
+    private double dYaw       = 0;
+
+    private double intakePower = 0.5;
+    private double raiseRelicPower = 0.4;
+    private double lowerRelicPower = -0.3;
+    private double extendRelicPower = 0.4;
+    private double withdrawRelicPower = -0.3;
+
+
+
+    @Override
+    public void init(){
+        initBase(); // From library, inits, sets direction, and resets all base motor.
+        initConveyor();
+        initGyro();
+        telemetry.addData("STATUS: ", "Initialised.");
+        telemetry.update();
+    }
+
+    @Override
+    public void init_loop(){
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop(){
+
+        updateGyro();
+        dAxial   = -gamepad1.left_stick_y;
+        dLateral =  gamepad1.left_stick_x;
+        dYaw     = -gamepad1.right_stick_x;
+        gyroYaw  = angles.firstAngle;
+
+        // Add some telemetry.
+        telemetry.addData("Axial: ", dAxial);
+        telemetry.addData("Lateral: ", dLateral);
+        telemetry.addData("Yaw: ", dYaw);
+        telemetry.addData("Gyro Yaw: ", gyroYaw);
+
+        setMoveRobot(dAxial, dLateral, dYaw);
+        moveConveyor(-gamepad2.right_stick_y);
+
+        if (gamepad2.right_trigger > 0.2){ // May need to change this.
+            moveIntake(intakePower);
+        }
+        else if (gamepad2.left_trigger > 0.2){
+            moveIntake(-intakePower);
+        }
+
+        if (gamepad2.right_bumper){
+            grabRelic(true);
+        }
+        else if (gamepad2.left_bumper){
+            grabRelic(false);
+        }
+
+        if (gamepad2.dpad_up){
+            raiseRelicArm(raiseRelicPower);
+        }
+        else if (gamepad2.dpad_down){
+            raiseRelicArm(lowerRelicPower);
+        }
+
+        if (gamepad2.dpad_right){
+            moveRelicArm(extendRelicPower);
+        }
+        else if (gamepad2.dpad_left){
+            moveRelicArm(withdrawRelicPower);
+        }
+
+        telemetry.update();
+    }
+
+    @Override
+    public void stop(){
+
+
+    }
+
+}
