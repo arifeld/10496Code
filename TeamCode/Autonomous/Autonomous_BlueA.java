7e6606be187c3ead0804e6b45820c951282e85ba
Rossington55 <harrosfeldos@gmail.com>
2017-12-08 19:19:40 +1100

    Update Autonomous_BlueA.java

--- TeamCode/Autonomous/Autonomous_BlueA.java
+++ TeamCode/Autonomous/Autonomous_BlueA.java
@@ -31,44 +31,54 @@ public class Autonomous_BlueA extends LibraryBaseAutonomous{
             // Add some telemetry.
             telemetry.addData("Current Time:", time.time());
 
+            if(time.time()<7.4){
+                kicker.setPosition(0.45);
 
-            if(time.time()<9){
-                kicker.setPosition(0.55);
+                if(time.time()<5){
+                    if(colour.red()<10){
+                        blueOnLeft = true;
+                        telemetry.addData("Blue location:", "Left");
+                    }else if(colour.red()>10){
+                        blueOnLeft = false;
+                        telemetry.addData("Blue location:", "Right");
+                    }else{
+                        telemetry.addData("Blue location:", "Unknown");
+                    }
+                }
 
-                if(colour.red()<75){
-                    blueOnLeft = false;
-                    telemetry.addData("Blue location:", "Right");
-                }else if(colour.red()>200){
-                    blueOnLeft = true;
-                    telemetry.addData("Blue location:", "Left");
-                }else{
-                    telemetry.addData("Blue location:", "Unknown");
+
+                if(time.time()>3.5 && time.time()<5){
+                    moveDropKick(0.1);
+                }else if(time.time()>5 && time.time()<7.4){
+                    moveDropKick(0);
                 }
             }else{
-                kicker.setPosition(0.95);
+                kicker.setPosition(0);
             }
 
 
             if(blueOnLeft){
                 if(time.time()>5 && time.time()<6){
-                    setMoveRobot(0.3,0,0);
+                    setMoveRobot(-0.25,0,0);
+                    telemetry.addData("I like", "spagety");
 
                 }else if(time.time()>6 && time.time()<7){
-                    setMoveRobot(-0.3,0,0);
+                    setMoveRobot(0.25,0,0);
 
-                }else if(time.time()>7 && time.time()<9){
-                    //moveDropKick(0.2);
+                }else if(time.time()>7 && time.time()<7.4){
+
                     setMoveRobot(0,0,0);
                 }
+
             }else{
                 if(time.time()>5 && time.time()<6){
-                    setMoveRobot(-0.3,0,0);
+                    setMoveRobot(0.25,0,0);
+                    telemetry.addData("I like", "nudls");
 
                 }else if(time.time()>6 && time.time()<7){
-                    setMoveRobot(0.3,0,0);
+                    setMoveRobot(-0.25,0,0);
 
-                }else if(time.time()>7 && time.time()<9) {
-                    // moveDropKick(0.2);
+                }else if(time.time()>7 && time.time()<7.4) {
                     setMoveRobot(0, 0, 0);
 
                 }
@@ -78,7 +88,6 @@ public class Autonomous_BlueA extends LibraryBaseAutonomous{
                 telemetry.addData("WORK?", vuMark);
 
 
-                telemetry.update();
                 if (vuMark == RelicRecoveryVuMark.RIGHT) {
                     vuMarkInt = 2;
 
@@ -94,25 +103,25 @@ public class Autonomous_BlueA extends LibraryBaseAutonomous{
 
                 if(vuMarkInt == 0){
                     telemetry.addData("Vumark", "left");
-                    if (time.time() > 11.5 && time.time() < 13) {
-                        setMoveRobot(0.5, 0, 0);
-                    } else if (time.time() > 13 && time.time() < 15.5) {
+                    if (time.time() > 11.5 && time.time() < 12.2) {
+                        setMoveRobot(-1, 0, 0);
+                    } else if (time.time() > 12.2 && time.time() < 15.5) {
                         setMoveRobot(0, 0, 0);
                     }
 
                 }else if(vuMarkInt ==1 ){
                     telemetry.addData("Vumark", "center");
-                    if (time.time() > 11.5 && time.time() < 12.5) {
-                        setMoveRobot(0.5, 0, 0);
-                    } else if (time.time() > 12.5 && time.time() < 15.5) {
+                    if (time.time() > 11.5 && time.time() < 12.3) {
+                        setMoveRobot(-0.5, 0, 0);
+                    } else if (time.time() > 12.3 && time.time() < 15.5) {
                         setMoveRobot(0, 0, 0);
                     }
                 }else if(vuMarkInt ==2){
 
                     telemetry.addData("Vumark", "right");
-                    if (time.time() > 11.5 && time.time() < 12) {
-                        setMoveRobot(0.5, 0, 0);
-                    } else if (time.time() > 12 && time.time() < 15.5) {
+                    if (time.time() > 11.5 && time.time() < 12.4) {
+                        setMoveRobot(-0.5, 0, 0);
+                    } else if (time.time() > 12.4 && time.time() < 15.5) {
                         setMoveRobot(0, 0, 0);
                     }
                 }
@@ -120,7 +129,6 @@ public class Autonomous_BlueA extends LibraryBaseAutonomous{
             else{
                 telemetry.addData("vuMark", "not found.");
             }
-            telemetry.update();
 
 
 
@@ -128,12 +136,12 @@ public class Autonomous_BlueA extends LibraryBaseAutonomous{
 
 
             //Turn the robot so the conveyor faces the cryptobox.
-            if(time.time()>16 && time.time()<16.5){
-                setMoveRobot(0,0,-1);
-            }else if(time.time()>16.6 && time.time()<18){
+            if(time.time()>16 && time.time()<16.8){
+                setMoveRobot(0,0,-0.5);
+            }else if(time.time()>16.8 && time.time()<18){
                 setMoveRobot(0,0,0);
             }else if(time.time()>18 && time.time()<25){
-                moveConveyor(0.5);
+                moveConveyor(0.05);
             }else if(time.time()>25){
                 moveConveyor(0);
             }
